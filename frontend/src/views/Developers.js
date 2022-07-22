7a4c3a5dbd5bab63cd8b159db8199bcde0ef55ea
Berlia <nehaberlia1@gmail.com>
2022-07-22 18:29:32 +0200

    done

--- frontend/src/views/Developers.js
+++ frontend/src/views/Developers.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import { MyContext } from "../components/Context/context.js";
->>>>>>> b301bf666133fb6d6c3b1ec322614b02de9bd192
 import { DevelopersComponent } from "../components/DevelopersComponent.jsx";
 
 
