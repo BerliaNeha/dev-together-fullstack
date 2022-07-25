45f8d27e73dbb466510983b9c84dc7fd857a7ba7
Sipter <v.zimara@gmail.com>
2022-07-25 15:47:32 +0200

    add dev community componet and connect the event/meetup/blog buttons with the dev community component

---
+++ frontend/src/components/DevCommunityComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const DevCommunityComponent = () => {
+    return( 
+        <div>hello</div>
+    )
+}
---
+++ frontend/src/views/DevCommunity.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { DevCommunityComponent } from "../components/DevCommunityComponent";
+import { Navbar } from "../components/Navbar.jsx";
+
+export const DevCommunity = () => {
+    return (
+        <>
+        <Navbar/>
+        <DevCommunityComponent/>
+        </>
+    )
+}
