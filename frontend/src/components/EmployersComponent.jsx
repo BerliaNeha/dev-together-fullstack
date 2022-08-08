dad86edaeb967caec9b570a6123e9656e49a352d
Berlia <nehaberlia1@gmail.com>
2022-08-08 15:44:42 +0200

    employers page added submit job

--- frontend/src/components/EmployersComponent.jsx
+++ frontend/src/components/EmployersComponent.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 import { Navbar } from "./Navbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -10,19 +12,32 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import { MyContext } from "../components/Context/context.js";
-
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
 
 export const EmployersComponent = () => {
-    const {  currentUserId} = React.useContext(MyContext);
+  const { currentUserId } = React.useContext(MyContext);
 
   const [username, setUsername] = useState("");
   const [open, setOpen] = React.useState(false);
+
+  const [shouldSubscribe, setShouldSubscribe] = React.useState(false);
+
+
   const handleClose = () => {
     setOpen(false);
   };
   const handleToggle = () => {
     setOpen(!open);
   };
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+    
+   
+  //   };
 
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -34,36 +49,38 @@ export const EmployersComponent = () => {
 
   useEffect(() => {
     const fetchUserData = async () => {
-        
-        const settings = {
-            method: "GET",
-            body: JSON.stringify(),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-        
-        const response = await fetch(process.env.REACT_APP_SERVER_URL + `/employers/${currentUserId}`, settings);
+      const settings = {
+        method: "GET",
+        body: JSON.stringify(),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
 
-        const parsedRes = await response.json();
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_URL + `/employers/${currentUserId}`,
+        settings
+      );
 
-        try {
-            // If the request was successful...
-            if (response.ok) {
-                console.log("Server response", parsedRes);
-                setUsername(parsedRes.username);
-                
-                // If the request was unsuccessful...
-            } else {
-                throw new Error(parsedRes.message);
-            }
-        } catch (err) {
-            alert(err.message);
+      const parsedRes = await response.json();
+
+      try {
+        // If the request was successful...
+        if (response.ok) {
+          console.log("Server response", parsedRes);
+          setUsername(parsedRes.username);
+
+          // If the request was unsuccessful...
+        } else {
+          throw new Error(parsedRes.message);
         }
-    }
+      } catch (err) {
+        alert(err.message);
+      }
+    };
 
     fetchUserData();
-}, [currentUserId])
+  }, [currentUserId]);
 
   return (
     <>
@@ -74,26 +91,181 @@ export const EmployersComponent = () => {
             <h2 id="greeting">Welcome {username}!</h2>
           </Item>
           <Item>
-            <div>
-              <Button onClick={handleToggle}>SUBMIT JOB</Button>
-              <Backdrop
+            <Container component="main" maxWidth="60%">
+              <CssBaseline />
+              <div>NEED TOP TALENT FOR YOUR TECH ROLE? GET STARTED NOW</div>
+              <Box
+                component="form" 
+                // validate onSubmit={handleSubmit}
                 sx={{
-                  color: "#fff",
-                  zIndex: (theme) => theme.zIndex.drawer + 1,
+                  boxShadow: "1px 1px 25px 4px #000000",
+                  borderRadius: 5,
+                  // "& > :not(style)": { m: 1, width: "150%" },
+                 
+                
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  opacity: 1,
+                  fontWeight: "bold",
+                  minWidth: "40%",
+                  margin: "auto",
+                  minHeight: "50vh",
+                 marginTop:6,
+                  backgroundColor: "rgba(35, 78, 112, 0.31)",
                 }}
-                open={open}
-                onClick={handleClose}
+                noValidate
+                autoComplete="off"
               >
-                <CircularProgress color="inherit" />
-              </Backdrop>
-            </div>
+                <TextField
+                  label="Company Name"
+                  variant="standard"
+                  id="companyName"
+                  type="text"
+                  name="companyName"
+                  sx={{ width: "80%" }}
+                  InputLabelProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 25,
+                      marginLeft: 12,
+                    },
+                  }}
+                  inputProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 20,
+                      marginLeft: 12,
+                    },
+                  }}
+                />
+
+                <TextField
+                  label="Email"
+                  variant="standard"
+                  id="email"
+                  type="email"
+                  name="email"
+                  sx={{ width: "80%" }}
+                  InputLabelProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 25,
+                      marginLeft: 12,
+                    },
+                  }}
+                  inputProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 20,
+                      marginLeft: 12,
+                    },
+                  }}
+                />
+
+                <TextField
+                  label="Position"
+                  variant="standard"
+                  id="position"
+                  type="position"
+                  name="position"
+                  sx={{ width: "80%" }}
+                  InputLabelProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 25,
+                      marginLeft: 12,
+                    },
+                  }}
+                  inputProps={{
+                    style: {
+                      fontWeight: "bold",
+                      color: "black",
+                      fontSize: 20,
+                      marginLeft: 12,
+                    },
+                  }}
+                />
+
+                <TextField
+                  id="standard-multiline-static"
+                  label="Job Description"
+                  multiline
+                  rows={10}
+                  col={10}
+                  variant="outlined"
+                  fullWidth
+                  sx={{ width: "80%", marginTop: 2 }}
+                  InputLabelProps={{
+                    style: { fontWeight: "bold", color: "black", fontSize: 25 },
+                  }}
+                  inputProps={{
+                    style: { fontWeight: "bold", color: "black", fontSize: 20 },
+                  }}
+                />
+                <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="primary"
+                    value={shouldSubscribe}
+                    onChange={() => setShouldSubscribe(!shouldSubscribe)}
+                  />
+                }
+                label="Subscribe for our newsletter"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="primary"
+                    required
+                    name="termsAndPolicy"
+                    value={true}
+                  />
+                }
+                label="I agree to and understand the Privacy Notice and Terms of Use"
+              />
+            </Grid>
+       
+
+                <Button onClick={handleToggle}>SUBMIT JOB</Button>
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                  }}
+                  open={open}
+                  onClick={handleClose}
+                >
+                  <CircularProgress color="inherit" />
+                </Backdrop>
+              </Box>
+            </Container>
           </Item>
-          <Item>Item 3</Item>
+          <Item>
+            <Button onClick={handleToggle}>SEARCH CVs</Button>
+            <Backdrop
+              sx={{
+                color: "#fff",
+                zIndex: (theme) => theme.zIndex.drawer + 1,
+              }}
+              open={open}
+              onClick={handleClose}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>
+          </Item>
         </Stack>
       </Box>
-      );
-      <div>Hello employer</div>
-      <div>Add job</div>
     </>
   );
 };
