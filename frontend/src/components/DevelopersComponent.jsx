e0bc41513a5cf8fb54e20a3a9891737db87ef08b
Sipter <v.zimara@gmail.com>
2022-08-11 15:39:17 +0200

    done

--- frontend/src/components/DevelopersComponent.jsx
+++ frontend/src/components/DevelopersComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "./Navbar";
 
 export const DevelopersComponent = () => {
-   
+//    4 fetch requests for education, experience, skills, languages
 
     return <>
     <Navbar/>
