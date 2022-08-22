63e436a5f2775ac8f7ffdbfab76b3085a6bad40f
Sipter <v.zimara@gmail.com>
2022-08-22 10:32:48 +0200

    update

--- backend/routes/cvRoute.js
+++ backend/routes/cvRoute.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { cvPost } from "../controllers/cvController.js";
-
+import { getAllCVs } from "../controllers/userDeveloperDataController.js";
 
 const CVrouter = express.Router();
 
-
 CVrouter.post("/", cvPost);
-
-
+CVrouter.get("/", getAllCVs);
 
 export default CVrouter;
