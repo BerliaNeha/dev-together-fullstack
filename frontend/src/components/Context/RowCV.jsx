56f339cd0f326a7932330d9dcf777ce39d030f8b
Sipter <v.zimara@gmail.com>
2022-09-05 09:26:44 +0200

    push the last changes

--- frontend/src/components/Context/RowCV.jsx
+++ frontend/src/components/Context/RowCV.jsx
@@ -1,14 +1,11 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
 import IconButton from "@mui/material/IconButton";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import { Card, Grid, Table, TableBody } from "@mui/material";
-import _ from "lodash";
 
 import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
